f0a5d85677c0ef7a146c2a572ca9beb2c5f519d1
254CMN <collinsnzei6@gmail.com>
2024-07-09 07:22:03 -0700

    First task completed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 #include<string.h>
+
 /**
- * @binary_to_uint - Function that converts binary to unsigned int
- * Return unsigned int if successful
+ * binary_to_uint - Function that converts binary to unsigned int
+ * Return 'sum' if successful
  * Return 0 if not successful
  *
  */
@@ -10,19 +11,22 @@ unsigned int binary_to_unit(const char *b)
 {
 	int sum = 0;
 	int i;
-	int j;
+	size_t j;
+	int n;
 
-	for (i = strlen(b); i <= 0; i--);
+	for (i = strlen(b); i <= 0; i--)
 	{
-		if (b[i] != '0' || b[i] != '1' || b == NULL);
-			return 0;
+		if (b[i] != '0' || b[i] != '1' || b == NULL)
+			return (0);
 		else
 		{
-			for (j = 0; j < strlen(b) ; j++);
+			for (j = 0; j < strlen(b) ; j++)
 			{
-				sum = b[i] * 2power j;
+				n = 1;
+				sum = sum + (b[i] * n;
 			}
+			n = n * 2;
 		}
 	}
-	return sum;
+	return (sum);
 }
