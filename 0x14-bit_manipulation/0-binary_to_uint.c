7a79c14a014ae47b093a664c326add0ef036dfb7
254CMN <collinsnzei6@gmail.com>
2024-07-10 04:22:16 -0700

    Corrections

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,25 +3,28 @@
 
 /**
  * binary_to_uint - Function that converts binary to unsigned int
- * @sum - Return value of function
- * @b - String of 0s amd 1s to be converted
+ * Return : unsigned int with decimal value of binary number, or 0 if error
+ * @b - pointer to a string of 0s and 1s 
  *
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int sum = 0;
-	int i;
-	int n = 1;
+       unsigned	int sum = 0;
+        int i;
 
-	for (i = strlen(b); i > 0; i--)
-	{
-		if (b[i] != '0' || b[i] != '1' || b == NULL)
-			return (0);
-		else
-		{
-			sum = sum + (b[i] * n);
-			n = n * 2;
-		}
-	}
-	return (sum);
+        if (!b)
+            return (0);
+        for (i = 0; b[i] != '\0'; i++)
+        {
+            if (b[i] != '0' && b[i] != '1')
+                return (0);
+            else
+            {
+                sum <<= 1;
+                if (b[i] == '1')
+                    sum += 1;
+            }
+        }
+        return (sum);
 }
+
