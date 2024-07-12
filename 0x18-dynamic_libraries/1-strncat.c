58ba02b28603bcbf2a4984dc4cdc53a4d93449bf
254CMN <collinsnzei6@gmail.com>
2024-07-12 04:01:12 -0700

    Creating a dynamic library and object files

--- 0x18-dynamic_libraries/1-strncat.c
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -6,5 +6,5 @@
 
 char * _strncat(char *dest, char *src, int n)
 {
-	return strncat(dest, src);
+	return strncat(dest, src, n);
 }
--- 0x18-dynamic_libraries/2-strlen.c
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#inlcude <ctype.h>
+#include <ctype.h>
 #include <string.h>
 
 int _strlen(char *s)
